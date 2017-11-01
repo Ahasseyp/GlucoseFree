505efa6d1c618698005339ae151695560bf156a2
Albert Hassey <ahasseyp97@gmail.com>
2017-10-31 18:54:42 -0600

    Add manual input function

--- js/my_jquery_functions.js
+++ js/my_jquery_functions.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   var today = new Date();
   var dow = today.getDay();
   var dd = today.getDate();
-  var mm = today.getMonth()+1;
+  var mm = today.getMonth();
 
   var weekday=new Array(7);
   weekday[1]="Monday";
@@ -17,40 +17,54 @@ $(document).ready(function(){
   var monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"]
 
+  $("#glevel_input").hide();
+
   $("#main_title").text(weekday[dow]+ ", "+monthNames[mm] + " " + dd);
 
   $("#assessment").click(function(){
-      $("#main_title").text("Assessment");
-      $('.demo-content > div').show();
-      $('.demo-content > div:not(#assessment-card)').hide();
+    $("#main_title").text("Assessment");
+    $('.demo-content > div').show();
+    $('.demo-content > div:not(#assessment-card)').hide();
   });
 
   $("#food").click(function(){
-      $("#main_title").text("Food");
-      $('.demo-content > div').show();
-      $('.demo-content > div:not(#food-card)').hide();
+    $("#main_title").text("Food");
+    $('.demo-content > div').show();
+    $('.demo-content > div:not(#food-card)').hide();
   });
 
   $("#emergency").click(function(){
-      $("#main_title").text("Emergency");
-      $('.demo-content > div').show();
-      $('.demo-content > div:not(#emergency-card)').hide();
+    $("#main_title").text("Emergency");
+    $('.demo-content > div').show();
+    $('.demo-content > div:not(#emergency-card)').hide();
   });
 
   $("#list-option-1").click(function(){
-      $("#food-graph").attr("fill", "#d81b60");
+    $("#food-graph").attr("fill", "#d81b60");
   });
 
   $("#list-option-2").click(function(){
-      $("#food-graph").attr("fill", "#f4511e");
+    $("#food-graph").attr("fill", "#f4511e");
   });
 
   $("#list-option-3").click(function(){
-      $("#food-graph").attr("fill", "#ffb300");
+    $("#food-graph").attr("fill", "#ffb300");
   });
 
   $("#list-option-4").click(function(){
-      $("#food-graph").attr("fill", "#388e3c");
+    $("#food-graph").attr("fill", "#388e3c");
+  });
+
+  $("#manual_input_button").click(function(){
+    $("#glevel_input").show();
+  });
+
+  $("#manual_input").on('keyup', function(e) {
+    if(e.keyCode == 13) {
+      $("#glevel_input").hide();
+      $("#glevel_int").text($('#manual_input').val());
+      $('#manual_input').val(null);
+    }
   });
 
 });
